9bcb217cbb3088ebfa0603c1ba497146a6b7a414
Harsh Dwivedi <hdwivedi@syr.edu>
2026-05-06 17:41:46 -0400

    Remove phone number from portfolio

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -23,6 +23,5 @@ export const heroData: HeroData = {
     github: 'https://github.com/harsh746-exe',
     linkedin: 'https://www.linkedin.com/in/harsh-dwivedi-854b39202/',
     email: 'edwivediharsh@gmail.com',
-    phone: '+1 (680) 216-2032',
   },
 };
